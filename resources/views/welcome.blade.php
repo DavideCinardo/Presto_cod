6ee96512f542700b1720d7651b8cdc485cfc4d64
Giuseppe Missanelli <missanelligiuseppe@gmail.com>
2022-11-25 16:25:15 +0100

    merge welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
 
                 </div>
             </div>
-        @endif
+        
             
             <div class="row justify-content-center">
                 @foreach($lastArticles as $lastArticle)
