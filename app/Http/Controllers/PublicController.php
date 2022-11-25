a2fe13b1a86052b76cbc012d606a8601fbe90473
Enzo Junior  Sacco <enzojunior@gmail.com>
2022-11-25 10:02:19 +0100

    Cambiamenti accettati

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
+
     public function homepage(){
-        return view ('welcome');
+        // mostra gli ultimi 3 articoli
+        $lastArticles = Article::all()->sortByDesc('created_at')->take(3);
+        
+        // passa gli articoli alla homepage
+        $articles = Article::all();
+        $articles = compact('articles');
+        return view ('welcome', compact('lastArticles'));
     }
+   
 
     public function category(Category $category){
         
