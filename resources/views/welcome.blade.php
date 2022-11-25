139782e71d9a4451abe9eca54f889236783674d4
Davide Cinardo <davide10cinardo@hotmail.it>
2022-11-25 10:46:24 +0100

    commit per la welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,16 @@
 <x-layout>
     
     <div class="container-fluid">
-        <div class="row justify-content-end">
+        <div class="row justify-content-center">
             <div class="col-12 col-md-3 min-vh-100">
-                <div class="SearchBar">
-                    <form class="d-flex my-3" role="search">
-                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                      <button class="btn btn-outline-success" type="submit">Search</button>
+                <div class="SearchBar d-flex flex-column-reverse">
+                    <form class=" my-3" role="search">
+                      <label for="search">Cosa cherchi?</label>
+                      <input class="form-control me-2 d-inline" name="search" type="search" placeholder="Macchina vintage, monete antiche..." aria-label="Search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     </form>
-                    <form class="d-flex my-3" role="search">
-                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                      <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                    <form class="d-flex my-3" role="search">
-                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                      <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+
                   </div>
             </div>
         </div>
