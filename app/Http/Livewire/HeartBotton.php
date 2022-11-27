87dc9fc05b6f0eafbfc90d59637be24fca202cb4
Nicola Cascella <nicocash96@gmail.com>
2022-11-27 14:14:10 +0100

    aggiornat tasti like

--- app/Http/Livewire/HeartBotton.php
+++ app/Http/Livewire/HeartBotton.php
@@ -8,15 +8,18 @@ use Illuminate\Support\Facades\Auth;
 class HeartBotton extends Component
 {
     public $article;
+    public $like;
 
     //funzione like
     public function like(){
         $this->article->users()->attach(Auth::user()->id);
+        $this->like = true;
     }
 
     //funzione dislike
     public function dislike(){
         $this->article->users()->detach(Auth::user()->id);
+        $this->like = false;
     }
 
     public function render()
--- resources/views/livewire/heart-botton.blade.php
+++ resources/views/livewire/heart-botton.blade.php
@@ -1,7 +1,3 @@
 <div>
-    @forelse($article->users as $user)
-        <button wire:click="{{$user->id == Auth::user()->id ? "dislike" : "like"}}" class="heart d-flex justify-content-center"><i class="{{$user->id == Auth::user()->id ? "text-danger fa-solid " : "fa-regular"}} fa-heart"></i></button>
-    @empty
-        <button wire:click="like" class="heart d-flex justify-content-center"><i class="fa-regular fa-heart"></i></button>
-    @endforelse
+    <button wire:click="{{$like == true ? "dislike" : "like"}}" class="heart d-flex justify-content-center"><i class="{{$like == true ? "text-danger fa-solid " : "fa-regular"}} fa-heart"></i></button>
 </div>
