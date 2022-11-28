7a78d20422c9d622c6697611b95ace6705b5ee59
Federico Pellegrino <federico.pellegrino994gmail.com>
2022-11-28 18:21:06 +0100

    cambiata navbar e reimplementate le funzioni

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,3 +10,30 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+  const navLogo = document.getElementById('navLogo');
+  let prev = window.pageYOffset;
+  let subNav = document.getElementById('subNav');
+
+  // window.addEventListener('scroll', function() {
+  //   let current = window.pageYOffset;
+  //   if (prev < current) {
+  //     navLogo.classList.add('hide');}
+  //     else{
+  //       navLogo.classList.remove('hide');
+  //     }
+  //     prev = current;
+  // })
+
+  window.onscroll = function(){
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+      navLogo.classList.add('d-none');
+      subNav.classList.add('bg-danger');
+      
+    } else {
+      navLogo.classList.remove('d-none');
+      subNav.classList.remove('bg-danger');
+    }
+  }
+  
