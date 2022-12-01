7f53275143cc705f0d09a259a0783fde474f87d0
Nicola Cascella <nicocash96@gmail.com>
2022-12-01 16:13:58 +0100

    cambiate scritte grigie in bianche, illuminati tasti verdi e rossi, dato sfondo a tasto like

--- resources/views/livewire/heart-botton.blade.php
+++ resources/views/livewire/heart-botton.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button wire:click="like_menager({{$article}})" type="submit" class="heart d-flex justify-content-center align-items-center border-0">
+    <button wire:click="like_menager({{$article}})" type="submit" class="heart d-flex justify-content-center align-items-center border-0 bg-white">
         <i class="{{Auth::user()->articleslike->contains($article->id) ? "text-danger fa-solid " : "fa-regular"}} fa-heart fs-4"></i>
     </button>
 </div>
